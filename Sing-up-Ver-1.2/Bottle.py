f97f13cc1604bf68d9390405307cf80c03df5bb2
Mose16 <47540314+Mose16@users.noreply.github.com>
2019-02-20 15:10:20 +1300

    Changed asthetics

---
+++ Sing-up-Ver-1.2/Bottle.py
@@ -0,0 +1,34 @@
+from bottle import run, route, view, get, post, request
+from itertools import count
+#LOGS
+#Ver-1.1 --- Added tickets list
+#Ver-1.2 --- Adding server functionality
+
+
+class Ticket:
+    _ids = count (0)
+    
+    def __init__(self,name,email,date_of_birth, check_in):
+        self.id = next(self._ids)
+        self.name = name
+        self.email = email
+        self.date_of_birth = date_of_birth
+        self.check_in = date_of_birth
+    
+
+TICKETS = [
+    Ticket("Moses", "moses@gmail.com", "16/11/2001", False),
+    Ticket("Jerry", "moses@gmail.com", "16/11/2001", False),
+    Ticket("Tom", "moses@gmail.com", "16/11/2001", False)
+]
+
+
+#Pages
+
+#Index page
+@route('/')
+@view('index')
+def index():
+    pass
+
+run(host='0.0.0.0', port = 8080, reloader = True, debug = True)
---
+++ Sing-up-Ver-1.2/README.md
@@ -0,0 +1,6 @@
+# Sing-up
+Sign-up
+
+#LOGS
+#Ver-1.1 --- Added tickets list
+#Ver-1.2 --- Adding server functionality
